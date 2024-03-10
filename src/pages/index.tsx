75dec86df707326d38b9f6a0e462ba1f8b89ee87
Mamenzul <victor.brettes@gmail.com>
2024-03-10 22:14:02 +0100

    checkbox to radio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,38 @@
 import Map from "@/components/Map/Map";
 import { Shell } from "@/components/shells/shell";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import villes from "@/components/jsons/villes.json";
-
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 const DEFAULT_CENTER = [46.228245, 2.1895145];
 
 export default function HomePage() {
-  const [one, setOne] = useState(false);
-  const [two, setTwo] = useState(false);
-  const [three, setThree] = useState(false);
-  const [four, setFour] = useState(false);
-  const [five, setFive] = useState(false);
-  const checkList = [
-    { id: 1, state: one },
-    { id: 2, state: two },
-    { id: 3, state: three },
-    { id: 4, state: four },
-    { id: 5, state: five },
-  ];
-  const checkListFiltered = checkList
-    .filter((el) => el.state === true)
-    .map((el) => el.id);
-  const filteredVilles = villes.filter((ville) =>
-    checkListFiltered.includes(Number(ville.z)),
-  );
-  console.log(filteredVilles);
+  const [id, setId] = useState("1");
+  const filteredVilles = villes.filter((ville) => ville.z === id);
   return (
     <Shell>
-      <div className="flex h-fit flex-col justify-between gap-2 md:flex-row">
+      <RadioGroup
+        className="flex h-fit flex-col justify-between md:flex-row"
+        defaultValue="1"
+        onValueChange={(e) => setId(e)}
+      >
         <div className="flex items-center space-x-2">
-          <Checkbox id="1" onCheckedChange={(e: boolean) => setOne(e)} />
+          <RadioGroupItem value="1" id="1" />
           <Label htmlFor="1">Zone très sous dotée</Label>
         </div>
         <div className="flex items-center space-x-2">
-          <Checkbox id="2" onCheckedChange={(e: boolean) => setTwo(e)} />
+          <RadioGroupItem value="2" id="2" />
           <Label htmlFor="2">Zone sous dotée</Label>
         </div>
         <div className="flex items-center space-x-2">
-          <Checkbox id="3" onCheckedChange={(e: boolean) => setThree(e)} />
-          <Label htmlFor="3">Zone intermédiaire</Label>
-        </div>
-        <div className="flex items-center space-x-2">
-          <Checkbox id="4" onCheckedChange={(e: boolean) => setFour(e)} />
+          <RadioGroupItem value="4" id="4" />
           <Label htmlFor="4">Zone sur dotée</Label>
         </div>
         <div className="flex items-center space-x-2">
-          <Checkbox id="5" onCheckedChange={(e: boolean) => setFive(e)} />
+          <RadioGroupItem value="5" id="5" />
           <Label htmlFor="5">Zone très sur dotée</Label>
         </div>
-      </div>
+      </RadioGroup>
       <Map
         className="h-full w-full"
         width="800"
