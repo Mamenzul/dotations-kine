55539889e820d89347bab362673247236be33841
Mamenzul <victor.brettes@gmail.com>
2024-03-11 15:11:10 +0100

    update header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="sticky top-0 z-30 w-full items-center justify-center border bg-white p-2 shadow-sm lg:mx-auto">
-      <div className="container flex h-16 items-center justify-center">
+      <div className="container flex items-center justify-center">
         <Link href="/">
-          <h1 className="font-header flex pl-3 pr-3 text-xl  font-semibold md:text-4xl ">
+          <h1 className="font-header flex pl-3 pr-3 text-xl  font-semibold md:text-xl ">
             DOTATIONS KINE
           </h1>
         </Link>
